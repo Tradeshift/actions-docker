e2174bf88ea161b7fff88d952b7501134450cd97
Arminio Andrei <arminio.andrei@tradeshift.com>
2023-07-20 13:50:03 +0300

    fix: ECR public registry uses us-east-1 region
    This commit fixes a bug related to ECR public registry
    which is available only in us-east-1 region.
    This is the error which will be fixed:
    ```
       Run tradeshift/actions-docker@v1
    💡 AWS Public ECR detected with eu-west-1 region
    ⬇️ Retrieving docker login password through AWS CLI 2.1.24 (/usr/local/bin/aws)...
    Error:
    Could not connect to the endpoint URL: "https://api.ecr-public.eu-west-1.amazonaws.com/"
    ```
    Snippet from [AWS documentation](https://docs.aws.amazon.com/AmazonECR/latest/public/getting-started-cli.html).
    ```
    aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws
    ```

--- __tests__/aws.test.ts
+++ __tests__/aws.test.ts
@@ -1,4 +1,4 @@
-import {isECRRepository} from '../src/aws';
+import {isECRRepository, getRegion} from '../src/aws';
 
 describe(isECRRepository, () => {
   it('returns true when given an ECR repository', () => {
@@ -22,5 +22,23 @@ describe(isECRRepository, () => {
     );
     expect(isECRRepository('test')).toEqual(false);
     expect(isECRRepository('test/test')).toEqual(false);
+  });
+});
+
+describe('getRegion', () => {
+  it('returns us-east-1 when given an ECR public repository', () => {
+    expect(getRegion('public.ecr.aws/tradeshift/my-image')).toEqual(
+      'us-east-1'
+    );
+  });
+  it('returns eu-west-1 when given an ECR private repository in eu-west-1', () => {
+    expect(
+      getRegion('123456789012.dkr.ecr.eu-west-1.amazonaws.com/my-image')
+    ).toEqual('eu-west-1');
+  });
+  it('returns cn-north-1 when given an ECR private repository in cn-north-1', () => {
+    expect(
+      getRegion('123456789012.dkr.ecr.cn-north-1.amazonaws.com.cn/my-image')
+    ).toEqual('cn-north-1');
   });
 });
